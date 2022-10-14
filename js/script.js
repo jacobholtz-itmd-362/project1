3a395cabc3fcf2f211b37992ef124845248eb44b
jacobholtz <87547604+jacobholtz@users.noreply.github.com>
2022-10-14 13:32:56 -0500

    changed a bit of code

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ function checkForm() {
     var firstName = document.forms["email_form"]["first_name"].value
     var lastName = document.forms["email_form"]["last_name"].value
     if ((!firstName) || (!lastName) || (!email)) {
-        alert("Please complete all the necessary fields!");
+        alert("Please complete all the necessary fields");
         return false;
     }
     else {
-        alert("You have successfully registered for our mailing list!")
+        alert("You have successfully registered to tour our school")
     }
 }
